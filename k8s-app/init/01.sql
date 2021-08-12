fdd36d5c18197053d6484fee2f7835e572ba95fa
kiljoss <kilian.ebi@kybeidos.de>
2021-08-12 18:05:55 +0200

    db-with local sql commands

--- k8s-app/init/01.sql
+++ k8s-app/init/01.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Chicago_Crimes_sample (
-    ID INT,
+    id INT,
     CaseNumber VARCHAR(8),
-    Date DATETIME,
+    Date VARCHAR(29),
     Block VARCHAR(22),
     IUCR VARCHAR(10),
     PrimaryType VARCHAR(26),
@@ -17,11 +17,12 @@ CREATE TABLE Chicago_Crimes_sample (
     XCoordinate INT,
     YCoordinate INT,
     YEAR INT,
-    UpdatedOn DATETIME,
+    UpdatedOn VARCHAR(29),
     Latitude INT,
     Longitude INT,
     LOCATION VARCHAR(50)
 );
+
 INSERT INTO Chicago_Crimes_sample (ID, CaseNumber, Date, Block, IUCR, PrimaryType, Description, LocationDescription, Arrest, Domestic, Beat, District, Ward, CommunityArea, FBICode, XCoordinate, YCoordinate, Year, UpdatedOn, Latitude, Longitude, Location) VALUES 
 (10817834,  'JA117989' ,  '01/16/2017 02:45:00 PM' ,  '033XX W OGDEN AVE' ,  '0880' ,  'THEFT' ,  'PURSE-SNATCHING' ,  'SIDEWALK' ,  'False' ,  'False' , 1021,  '10.0' , '24.0',  '29.0' ,  '06' , NULL, NULL, 2017,  '01/23/2017 03:52:03 PM' , NULL, NULL, NULL),
 (10817835,  'JA118386' ,  '01/16/2017 04:30:00 PM' ,  '069XX S CORNELL AVE' ,  '0610' ,  'BURGLARY' ,  'FORCIBLE ENTRY' ,  'APARTMENT' ,  'False' ,  'False' , 332,  '3.0' , '5.0',  '43.0' ,  '05' , NULL, NULL, 2017,  '01/23/2017 03:52:03 PM' , NULL, NULL, NULL);
