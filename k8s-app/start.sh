d145cb97001b92bdac7c002bfd5e852ca2d5ca52
kiljoss <kilian.ebi@kybeidos.de>
2021-08-13 20:27:28 +0200

    webappyaml

--- k8s-app/start.sh
+++ k8s-app/start.sh
@@ -3,6 +3,6 @@ kubectl apply -f 02-k8s-mysql-service.yaml
 kubectl apply -f 03-memcached-deployment-and-service.yaml
 kubectl apply -f 04-app-deployment-minikube.yaml
 kubectl apply -f 04-app-service-and-ingress.yaml
-kubectl apply -f deployment.yaml
+kubectl apply -f webapp.yaml
 kubectl scale deployment memcache-deployment --replicas=2
 kubectl scale deployment my-super-app-deployment --replicas=2
