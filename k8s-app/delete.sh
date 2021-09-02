1b0214ccfa3cf6da3e2e57381aecdf5d50de6cbc
kiljos <you@example.com>
2021-09-02 22:18:53 +0200

    deleted some files (dok comp)

--- k8s-app/delete.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete service/bigdata-webapp-service
-kubectl delete service/my-app-mysql-service
-kubectl delete deployment.apps/bigdata-webapp
-kubectl delete deployment.apps/mysql-deployment
--- k8s-app/start.sh
+++
@@ -1,6 +0,0 @@
-kubectl apply -f db_mysql.yaml
-sleep 60
-kubectl apply -f memcached.yaml
-sleep 60
-kubectl apply -f webapp.yaml
-
