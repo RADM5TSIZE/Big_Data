2b7b5150569a32990892f53e604a34b8a614f291
kiljos <63229058+kiljos@users.noreply.github.com>
2021-09-03 10:07:12 +0200

    deleted demo

--- k8s-demo-app/delete.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete deployment/memcache-deployment
-kubectl delete deployment/my-super-app-deployment
-kubectl delete deployment/mysql-deployment
-kubectl delete svc/my-super-app-service
-kubectl delete svc/my-memcached-service
-kubectl delete svc/my-app-mysql-service
-kubectl delete ingress/my-super-app-ingress
